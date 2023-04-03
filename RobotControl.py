96d55644a3b7b0a6df0a188559a2a7b74b14e3f5
Aiden Tersine <aiden.tersine.20@cnu.edu>
2023-04-03 12:10:38 -0400

    setup robot control methods

--- RobotControl.py
+++ RobotControl.py
@@ -1,11 +1,27 @@
 import ImageTracer
 
 
-def draw_image():
-    return "placeholder"
+# draws every curve in an image
+def draw_image(path):
+    for curve in path:
+        draw_curve(curve)
 
-def draw_line():
-    return "placeholder"
+
+# draws a single curve
+def draw_curve(curve):
+    start = curve.start_point
+    for segment in curve:
+        x0, y0 = start
+        if segment.is_corner:
+            x1, y1 = segment.c
+            x2, y2 = segment.end_point
+            # draw segment
+        else:
+            x1, y1 = segment.c1
+            x2, y2 = segment.c2
+            x3, y3 = segment.end_point
+            # draw segment
+        start = segment.end_point
 
 
 if __name__ == "__main__":
